a9ddbbcb49dd76b6d119646a5a066ee123ae2b8e
Sarkissian-Telmane <telmane.sarkissian@edu.univ-fcomte.fr>
2025-11-18 17:00:28 +0100

    défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,15 @@
-// *** CAROUSEL ***
-// TODO
+const carousel = document.querySelector('.carousel__container');
+const nextButton = document.querySelector(".carousel__button--next");
+const prevButton = document.querySelector(".carousel__button--prev");
+
+if (carousel) {
+    prevButton.addEventListener("click", (evt) => {
+        carousel.scrollBy({ left: -260, behavior: "smooth" });
+    })
+    nextButton.addEventListener("click", (evt) => {
+        carousel.scrollBy({ left: +260, behavior: "smooth" });
+    })
+}
+
+console.log("prevButton :", prevButton);
+console.log("nextButton :", nextButton);
