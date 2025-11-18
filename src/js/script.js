5b6b4e7755af735dc38a74b7499cd224d680a515
Sarkissian-Telmane <telmane.sarkissian@edu.univ-fcomte.fr>
2025-11-18 17:08:11 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,13 @@
 const carousel = document.querySelector('.carousel__container');
 const nextButton = document.querySelector(".carousel__button--next");
 const prevButton = document.querySelector(".carousel__button--prev");
+const premierItem = document.querySelector(".carousel__item");
 
 if (carousel) {
     prevButton.addEventListener("click", (evt) => {
-        carousel.scrollBy({ left: -260, behavior: "smooth" });
+        carousel.scrollBy({ left: -premierItem.clientWidth, behavior: "smooth" });
     })
     nextButton.addEventListener("click", (evt) => {
-        carousel.scrollBy({ left: +260, behavior: "smooth" });
+        carousel.scrollBy({ left: +premierItem.clientWidth, behavior: "smooth" });
     })
 }
-
-console.log("prevButton :", prevButton);
-console.log("nextButton :", nextButton);
